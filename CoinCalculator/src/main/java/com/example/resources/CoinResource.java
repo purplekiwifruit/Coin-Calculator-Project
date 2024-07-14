9001d7ac10f3dcb99dc071fa273f5b307fc148c8
purplekiwifruit <148666036+purplekiwifruit@users.noreply.github.com>
2024-07-14 12:24:33 +0800

    add more test case and validation at server side

--- CoinCalculator/src/main/java/com/example/resources/CoinResource.java
+++ CoinCalculator/src/main/java/com/example/resources/CoinResource.java
@@ -1,6 +1,7 @@
 package com.example.resources;
 
 
+import com.example.core.BadRequestException;
 import com.example.core.CoinChangeService;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -21,7 +22,22 @@ public class CoinResource {
 
     @POST
     public List<Double> getCoin(CoinChange coinChange) {
+        validateInput(coinChange);
         return CoinChangeService.getCoins(coinChange.coinDenominators, coinChange.targetAmount);
+    }
+
+    private void validateInput(CoinChange coinChange) {
+        if (coinChange.coinDenominators == null) {
+            throw new BadRequestException("Coin denominators cannot be null");
+        }
+
+        if (coinChange.targetAmount <= 0.0 || coinChange.targetAmount > 10000.0) {
+            throw new BadRequestException("Target amount must be between 0.01 and 10000.00");
+        }
+
+        if (Math.round(coinChange.targetAmount * 100) != coinChange.targetAmount * 100) {
+            throw new BadRequestException("Target amount must have no more than two decimal places");
+        }
     }
 
 }
